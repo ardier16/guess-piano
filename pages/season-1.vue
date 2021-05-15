335bf097a7eed39dd03247f61c1ca875fb903763
ardier16 <ardier16@gmail.com>
2021-05-16 01:48:43 +0300

    Add season 1 page

---
+++ pages/season-1.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="season-1">
+    <div>
+      <h1 class="season-1__title">
+        #GUESSPIANO Сезон 1
+      </h1>
+
+      <div class="season-1__episodes">
+        <div
+          v-for="(episode, i) in seasonData.episodes"
+          :key="i"
+          class="season-1__episode"
+        >
+          {{ episode }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import season1Data from '@/data/season-1.json'
+
+export default Vue.extend({
+  computed: {
+    seasonData: () => season1Data
+  }
+})
+</script>
+
+<style>
+.season-1 {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.season-1__title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
